70c782f49b107aeb8df5ccfd9de72a4ccc7fa2a5
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2021-11-08 22:22:10 +0530

    1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-// navbar scrolling
-const body = document.body;
-const navbar = document.getElementById("navbar");
-let previousScroll = 0;
-
-window.addEventListener("scroll", () => {
-  currentScroll = window.pageYOffset;
-
-  //   stick to top class
-  if (currentScroll == 0) {
-    navbar.classList.add("stick-to-top");
-  } else {
-    navbar.classList.remove("stick-to-top");
-  }
-
-  previousScroll = currentScroll;
-});
+$("#navigation").load("/components/nav.html");
