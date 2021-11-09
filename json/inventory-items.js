dd99529e8656de15ecd9af887b51b4a5a085d743
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2021-11-09 13:16:00 +0530

    Inventory Completed

---
+++ json/inventory-items.js
@@ -0,0 +1,185 @@
+/**
+ * ? POINTS TO REMEMBER
+ * 1. Add comma after every entry
+ * 2. Dont add comma after last entry
+ * 3. Use these category value only
+ *      -- head
+ *      -- eyes
+ *      -- lower-face
+ *      -- neck
+ *      -- cape
+ *      -- background
+ *      -- hand
+ *      -- extra-finger
+ */
+
+const items = [
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "basic",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+  {
+    name: "Head Cap",
+    image: "/assets/nfts/head-cap.jpeg",
+    rarity: "common",
+    category: "head",
+  },
+  {
+    name: "Cape Wings",
+    image: "/assets/nfts/cape-wings.jpeg",
+    rarity: "premium",
+    category: "cape",
+  },
+  {
+    name: "Neck Scarf",
+    image: "/assets/nfts/neck-scarf.jpeg",
+    rarity: "common",
+    category: "neck",
+  },
+  {
+    name: "Extra Finger",
+    image: "/assets/nfts/extra-6th.jpeg",
+    rarity: "rare",
+    category: "extra-finger",
+  },
+];
